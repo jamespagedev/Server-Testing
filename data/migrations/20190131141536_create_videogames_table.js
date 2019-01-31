2661f0602742b0b8178bc4e1979ff75a4abc7d6e
jamespagedev <jamespagedev@outlook.com>
2019-01-31 14:32:19 -0700

    can't create test database... moving on

---
+++ data/migrations/20190131141536_create_videogames_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('videogames', function(tbl) {
+    // Primary Key 'id'
+    tbl.increments();
+
+    // Other Columns
+    tbl
+      .string('name', 255)
+      .notNullable()
+      .unique();
+    tbl.text('platform').notNullable();
+    tbl.boolean('completed').defaultTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropUnique('name').dropTableIfExists('videogames');
+};
