714d9e093513fb2243be03f7459a2186bc24825b
jamespagedev <jamespagedev@outlook.com>
2019-01-31 17:10:23 -0700

    Testset: add3/getAll the 3 videogames completed

--- data/helpers/__tests__/videogamesModel.js
+++ data/helpers/__tests__/videogamesModel.js
@@ -6,15 +6,19 @@ afterAll(async () => {
   await db('videogames').truncate();
 });
 
-/* Testsuite: videogamesModel
-    - Testset: add/get/edit/get/remove the videogame
-      - Testcase: add videogame
-      - Testcase: get videogame that was just added
-      - Testcase: edit videogame name
-      - Testcase: get videogame that was just edited
-      - Testcase: remove the videogame from database
-    - Testset: add3/getAll the 3 videogames
-    - More Ideas to implement after MVP:
+/* [X] Testsuite: videogamesModel
+    - [X] Testset: add/get/edit/get/remove the videogame
+      - [X] Testcase: add videogame
+      - [X] Testcase: get videogame that was just added
+      - [X] Testcase: edit videogame name
+      - [X] Testcase: get videogame that was just edited
+      - [X] Testcase: remove the videogame from database
+    - [X] Testset: add3/getAll the 3 videogames
+      - [X] Testcase: add videogame
+      - [X] Testcase: add videogame
+      - [X] Testcase: add videogame
+      - [X] Testcase: get all video games
+    - (Optional): More Ideas to implement after MVP:
       - Add testcases to check if entry has all required fields
 */
 
@@ -105,6 +109,75 @@ describe('Testsuite: videogamesModel', () => {
 
       // Result(s)
       expect(videogamesQuery).toHaveLength(0);
+
+      // Cleanup Testset
+      await db('videogames').truncate();
+    });
+  });
+
+  // Testset
+  describe('Testset: add/get/edit/get/remove the videogame', () => {
+    // Testcase
+    it('Testcase: add first videogame', async () => {
+      // Setup
+      const videogame = {
+        name: 'Chronno Trigger',
+        platform: 'snes',
+        completed: true
+      };
+
+      // Action(s)
+      await videogamesModel.addVideoGame(videogame);
+
+      // Result(s)
+      const videogameQuery = await videogamesModel.findById(1);
+      expect(videogameQuery.id).toBe(1);
+      expect(videogameQuery.name).toBe(videogame.name);
+      expect(videogameQuery.platform).toBe(videogame.platform);
+      // Use !! to turn boolean from number 0/1 to boolean true/false
+      expect(!!videogameQuery.completed).toBe(videogame.completed);
+    });
+
+    // Testcase
+    it('Testcase: add second videogame', async () => {
+      // Setup
+      const videogame = {
+        name: 'Parasite Eve',
+        platform: 'ps1',
+        completed: true
+      };
+
+      // Action(s)
+      await videogamesModel.addVideoGame(videogame);
+
+      // Result(s)
+      const videogameQuery = await videogamesModel.findById(2);
+      expect(videogameQuery.id).toBe(2);
+      expect(videogameQuery.name).toBe(videogame.name);
+      expect(videogameQuery.platform).toBe(videogame.platform);
+      // Use !! to turn boolean from number 0/1 to boolean true/false
+      expect(!!videogameQuery.completed).toBe(videogame.completed);
+    });
+
+    // Testcase
+    it('Testcase: add third videogame', async () => {
+      // Setup
+      const videogame = {
+        name: 'Xenogears',
+        platform: 'ps1',
+        completed: true
+      };
+
+      // Action(s)
+      await videogamesModel.addVideoGame(videogame);
+
+      // Result(s)
+      const videogameQuery = await videogamesModel.findById(3);
+      expect(videogameQuery.id).toBe(3);
+      expect(videogameQuery.name).toBe(videogame.name);
+      expect(videogameQuery.platform).toBe(videogame.platform);
+      // Use !! to turn boolean from number 0/1 to boolean true/false
+      expect(!!videogameQuery.completed).toBe(videogame.completed);
     });
   });
 });
